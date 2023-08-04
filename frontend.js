0fdf5486449b899cb202826d6bfbb7909552e3ef
dean840513 <50709683+dean840513@users.noreply.github.com>
2023-08-04 15:57:39 +0800

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("ws://192.168.1.128:8080");
+const ws = new WebSocket("ws://localhost:8080");
 
 ws.onopen = function(){
 	document.getElementById('status').innerText = '已连接';
